d348fa10fbdbf65a3d1cd0b30043b8e5ce06febd
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2025-08-12 07:33:07 -0300

    Update library.onstart(1).lua
    fix obvious mistake with O2 and H1.

--- lua_sources/library.onstart(1).lua
+++ lua_sources/library.onstart(1).lua
@@ -4,12 +4,12 @@ SlotList = {slot1, slot2, slot3, slot4, slot5, slot6, slot7, slot8, slot9, slot1
 
 OreList = {}
 OreList.T1 = {}
-OreList.T1.hydrogen  =  1010524904
-OreList.T1.oxygen    =  947806142
-OreList.T1.iron 	=  198782496
 OreList.T1.aluminum  =  2240749601
 OreList.T1.carbon 	=  159858782
+OreList.T1.iron 	=  198782496
 OreList.T1.silicon 	=  2589986891
+-- X OreList.T1.hydrogen  =  1010524904
+-- X OreList.T1.oxygen    =  947806142
 
 
 OreList.T2 = {}
@@ -18,7 +18,7 @@ OreList.T2.copper	=  1466453887
 OreList.T2.sodium 	=  3603734543
 OreList.T2.calcium 	=  2112763718
 
-OreList.Default = OreList.T1.oxygen
+OreList.Default = OreList.T1.aluminum
 
 IndustryStatus = {}
 IndustryStatus.jammed	= 3
