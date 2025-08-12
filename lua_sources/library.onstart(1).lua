75d7173a4f153b1fa462315dd281e70a0c32223c
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2025-08-12 07:46:42 -0300

    Update library.onstart(1).lua
     alternative use case example : smelter management

--- lua_sources/library.onstart(1).lua
+++ lua_sources/library.onstart(1).lua
@@ -8,9 +8,6 @@ OreList.T1.aluminum  =  2240749601
 OreList.T1.carbon 	=  159858782
 OreList.T1.iron 	=  198782496
 OreList.T1.silicon 	=  2589986891
--- X OreList.T1.hydrogen  =  1010524904
--- X OreList.T1.oxygen    =  947806142
-
 
 OreList.T2 = {}
 OreList.T2.chromium =  2147954574
@@ -19,6 +16,19 @@ OreList.T2.sodium 	=  3603734543
 OreList.T2.calcium 	=  2112763718
 
 OreList.Default = OreList.T1.aluminum
+
+--- # alternative use case example # ---
+
+ProductList = {}
+ProductList.T1 = {}
+ProductList.T1.AlFeAlloy = 18262914
+ProductList.T1.silumin = 2565702107
+ProductList.T1.steel =  511774178
+
+ProductList.T2 = {}
+ProductList.T2.CalciumReinforcedCopper = 1034957327
+ProductList.T2.StainlessSteel = 2984358477
+ProductList.T2.Duralumin = 231758472
 
 IndustryStatus = {}
 IndustryStatus.jammed	= 3
